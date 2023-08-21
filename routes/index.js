0b75517da568f0c5ea5d499c13d791bd6e345538
David Timofeev <dava.tim@yandex.ru>
2023-08-22 00:08:04 +0300

    d.1r

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,17 @@ const movieRouter = require("./movies");
 
 const { auth } = require("../middlewares/auth");
 const { login, createUser } = require("../controllers/users");
-const ERROR_404_NOTFOUND = require("../errors/ERROR_404_NOTFOUND");
 const ANAUTHORUZED_REQUEST_401 = require("../errors/ANAUTHORUZED_REQUEST_401");
 
 const validation = require("../middlewares/validation");
 
 router.post("/signin", validation.login, login);
 router.post("/signup", validation.createUser, createUser);
-
-router.use("/users", auth, userRouter);
-router.use("/movies", auth, movieRouter);
+router.use(auth);
+router.use("/users", userRouter);
+router.use("/movies", movieRouter);
 
 router.use((_req, _res, next) => {
-  // next(new ERROR_404_NOTFOUND("Ошибка 404 - страница не найдена"));
   next(new ANAUTHORUZED_REQUEST_401("Сначала авторизуйтесь"));
 });
 
