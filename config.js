d4c6ef72761341f3a0a6e8758906d0823246edbb
David Timofeev <dava.tim@yandex.ru>
2023-08-21 22:30:08 +0300

    Смена порта v2

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const { NODE_ENV, PORT, JWT_SECRET, CONNECT } = process.env;
 
 const config = {
   nodeEnv: NODE_ENV || "development",
-  port: PORT || 3000,
+  port: PORT || 4000,
   jwtSecret: NODE_ENV === "production" ? JWT_SECRET : "some-secret-key",
   connectDb: CONNECT || "mongodb://127.0.0.1:27017/bitfilmsdb",
 };
