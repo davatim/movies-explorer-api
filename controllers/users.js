747d33940287fa1270a8d5dd014ea701f756bf6c
David Timofeev <dava.tim@yandex.ru>
2023-09-06 20:41:37 +0300

    add cors

--- controllers/users.js
+++ controllers/users.js
@@ -55,10 +55,13 @@ module.exports.login = (req, res, next) => {
         if (!isEqual) {
           throw new ANAUTHORUZED_REQUEST_401('Не правильная почта или пароль');
         }
-        const token = jwt.sign({ _id: user._id }, config.jwtSecret, {
-          expiresIn: '7d',
+        const token = jwt.sign({ _id: user._id }, 'super-secret-kei', { expiresIn: '7d' });
+        res.cookie('jwt', token, {
+          maxAge: 604800,
+          httpOnly: true,
+          sameSite: true,
         });
-        return res.status(200).send({ token });
+        return res.status(INFO_200_SEC_SEND).send(user);
       });
     })
     .catch(next);
