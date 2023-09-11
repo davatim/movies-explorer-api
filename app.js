f894fd0bb974fc638584fd30ec3d1707f5599dfb
David Timofeev <dava.tim@yandex.ru>
2023-09-11 23:56:45 +0300

    change cors to 4000 port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 // app.use(cors());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://davatimdiplom.nomoredomainsicu.ru'],
+    origin: ['http://localhost:4000', 'http://davatimdiplom.nomoredomainsicu.ru'],
     credentials: true,
     maxAge: 3000000000,
   }),
